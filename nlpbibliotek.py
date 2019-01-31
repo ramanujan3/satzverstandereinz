a257dde4f979bb0c5deaa76083a2b7891fb234f9
ramanujan3 <ramanujan3@gmail.com>
2019-01-31 17:34:43 -0500

    nlpbib

---
+++ nlpbibliotek.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+import gensim
+from nltk.tokenize import word_tokenize
+import numpy as np
+
+
+"""
+Zeichenen = Tokenize
+Vonfeld = From_field
+Anfeld = To_field
+Bauen worter modell = build words model
+Anhangen wortcodes = Add word codes
+"""
+
+
+def zeichenen(daten_pd, vonfeld, anfeld):
+
+    # daten_pd[anfeld] = daten_pd.apply(lambda row: word_tokenize(row[vonfeld]), axis=1)
+    daten_pd[anfeld] = daten_pd.apply(lambda row: row[vonfeld]
+                                      .replace('"', '').replace("'", "")
+                                      .replace(',', ' ').replace(':', ' ').replace('.', ' ')
+                                      .replace('-', ' ').replace('|', ' ').replace('/', ' ')
+                                      .replace('#', ' ').replace('&', ' ').replace('+', ' ')
+                                      .replace(')', ' ').replace('(', ' ').lower()
+                                      .split(' '), axis=1)
+    # daten_pd[anfeld] = daten_pd[vonfeld].split(' ')
+    return daten_pd
+
+
+def bauen_wortermodell(sentences):
+    model = gensim.models.Word2Vec(sentences, min_count=1)
+    return model
+
+
+def anhangen_wortcodes(daten_pd, wortermodell, vonfeld, anfeld):
+
+    wort_seq_list = daten_pd[vonfeld]
+    wort_cseq_list = []
+    # print 'wort_seq_list', wort_seq_list
+
+    for i, wort_seq in enumerate(wort_seq_list):
+
+        wort_cseq = []
+        # codes_avg = None
+        for j, wort in enumerate(wort_seq):
+
+            wort_code = np.array(wortermodell[wort])
+            wort_cseq.append(wort_code)
+            # print i, j, 'wort_code', wort, wort_code
+
+            # if (codes_avg is None):
+            #     codes_avg = wort_code
+            # else:
+            #     codes_avg = codes_avg + wort_code
+        # codes_avg = codes_avg / len(wort_seq)
+
+        wort_cseq_list.append(wort_cseq)
+
+    daten_pd[anfeld] = wort_cseq_list
+    return daten_pd
--- satzverstander.py
+++ satzverstander.py
@@ -22,8 +22,18 @@ Die Unterlagen = the docs
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-vz', '--vzchn', type=str, dest='vzchn', default='../matching_model/data/cl3.csv',
+    parser.add_argument('-vz', '--vzchn', nargs='+', type=str, dest='vzchn',
+                        default=['../matching_model/data/cl1.csv',
+                                 '../matching_model/data/cl2.csv',
+                                 '../matching_model/data/cl3.csv'],
                         help='verzeichnis zu laden')
+
+    ############################
+    # TBD:
+    #  NOT SURE ABOUT "ITEM TEXT" and "OBJECT"
+    parser.add_argument('-pn', '--proname', nargs='+', type=str, dest='proname',
+                        default=['Item Text', 'Object', 'Procurement Name'],
+                        help='procurement namen zu laden')
 
     return parser.parse_args()
 
@@ -32,21 +42,24 @@ def main():
 
     args = parse_args()
 
-    daten_pd = dl.laden_unterlagen(args.vzchn, ['Procurement Name', 'Implan536Index'])
+    for i, vzchn in enumerate(args.vzchn):
+        daten_pd = dl.laden_unterlagen(vzchn, [args.proname[i], 'Implan536Index'])
 
-    # print daten_pd['Procurement Name']
+        # print daten_pd['Procurement Name']
+        nl.zeichenen(daten_pd, args.proname[i], 'Procurement Zeichen')
 
-    nl.zeichenen(daten_pd, 'Procurement Name', 'Procurement Zeichen')
     wortermodell = nl.bauen_wortermodell(daten_pd['Procurement Zeichen'])
 
     worter = list(wortermodell.wv.vocab)
     worter.sort()
-    print(worter)
-    print(len(worter))
+    # print(worter)
+    print('Number of Words:',
+          len(worter))
 
     nl.anhangen_wortcodes(daten_pd, wortermodell, 'Procurement Zeichen', 'Procurement Coden')
 
-    print(daten_pd['Procurement Coden'])
+    print('Number of Procurement lines:',
+          len(daten_pd['Procurement Coden']))
     # print(wortermodell['aramark'])
 
     print('Done!')
