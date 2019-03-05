5330d538c3f28dd1c6363af15247361b1fbce3ff
Ramanujan3 <ramanujan3@gmail.com>
2019-03-05 15:41:47 -0500

    yesterday

--- nlpbibliotek.py
+++ nlpbibliotek.py
@@ -24,14 +24,15 @@ def zeichenen(daten_pd, vonfeld, anfeld):
                                       .replace(',', ' ').replace(':', ' ').replace('.', ' ')
                                       .replace('-', ' ').replace('|', ' ').replace('/', ' ')
                                       .replace('#', ' ').replace('&', ' ').replace('+', ' ')
+                                      .replace('«', ' ').replace('–', ' ')
                                       .replace(')', ' ').replace('(', ' ').lower()
-                                      .split(' '), axis=1)
+                                      .split(), axis=1)
     # daten_pd[anfeld] = daten_pd[vonfeld].split(' ')
     return daten_pd
 
 
-def bauen_wortermodell(sentences):
-    model = gensim.models.Word2Vec(sentences, min_count=1)
+def bauen_wortermodell(sentences, vec_size, vec_wind):
+    model = gensim.models.Word2Vec(sentences, size=vec_size, window=vec_wind, min_count=1)
     return model
 
 
@@ -40,6 +41,36 @@ def anhangen_wortcodes(daten_pd, wortermodell, vonfeld, anfeld):
     wort_seq_list = daten_pd[vonfeld]
     wort_cseq_list = []
     # print 'wort_seq_list', wort_seq_list
+
+    for i, wort_seq in enumerate(wort_seq_list):
+
+        wort_cseq = []
+        # codes_avg = None
+        for j, wort in enumerate(wort_seq):
+
+            wort_code = np.array(wortermodell[wort])
+            wort_cseq.append(wort_code)
+            # print i, j, 'wort_code', wort, wort_code
+
+            # if (codes_avg is None):
+            #     codes_avg = wort_code
+            # else:
+            #     codes_avg = codes_avg + wort_code
+        # codes_avg = codes_avg / len(wort_seq)
+
+        wort_cseq_list.append(wort_cseq)
+
+    daten_pd[anfeld] = wort_cseq_list
+    return daten_pd
+
+
+def bauen_ausbildungen(daten_pd, wortermodell, vonfeld, neuefeld):
+
+
+    wort_seq_list = daten_pd[vonfeld]
+    wort_cseq_list = []
+    # print 'wort_seq_list', wort_seq_list
+
 
     for i, wort_seq in enumerate(wort_seq_list):
 
--- satzverstander.py
+++ satzverstander.py
@@ -28,18 +28,33 @@ Die Unterlagen = the docs
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-vz', '--vzchn', nargs='+', type=str, dest='vzchn',
-                        default=['~/matching_model/data/cl1.csv',
-                                 '~/matching_model/data/cl2.csv',
-                                 '~/matching_model/data/cl3.csv'],
+                        default=['~/matching_model/data/cl3.csv'],
+                                 # '~/matching_model/data/cl1.csv',
+                                 # '~/matching_model/data/cl2.csv',
+                                 # '~/matching_model/data/cl3.csv'],
                         help='verzeichnis zu laden')
 
-    parser.add_argument('-pt', '--protext', nargs='+', type=str, dest='protext',
-                        default=['Item Text', 'AP 3rd Level', 'Procurement Name'],
+    parser.add_argument('-pt', '--protexts', nargs='+', type=str, dest='protexts',
+                        default=[['Procurement Name', 'GL Name']],
+                        		 # 'Item Text', 
+                        		 # 'AP 3rd Level',
+                        		 # 'Procurement Name'],
                         help='procurement namen zu laden')
 
     parser.add_argument('-pc', '--proclass', nargs='+', type=str, dest='proclass',
-                        default=['Implan536Index', 'Implan536Index', 'Implan536Index'],
+                        default=['Implan536Index'],
+                      			 # 'Implan536Index',
+                                 # 'Implan536Index',
+                                 # 'Implan536Index'],
                         help='procurement namen zu laden')
+
+    parser.add_argument('-vg', '--vekgrosse', type=int, dest='vek_grosse',
+                        default=30,
+                        help='Grosse des Vektors')
+
+    parser.add_argument('-vf', '--vekfenster', type=int, dest='vek_fenster',
+                        default=3,
+                        help='Fenster des Vektors')
 
     return parser.parse_args()
 
@@ -49,26 +64,45 @@ def main():
     args = parse_args()
 
     for i, vzchn in enumerate(args.vzchn):
-        daten_pd = dl.laden_unterlagen(vzchn, [args.protext[i], 'Implan536Index'])
+
+        cols_zu_laden = args.protexts[i] + [args.proclass[i]]
+        print(args.protexts[i])
+        print(args.proclass[i])
+        print(cols_zu_laden)
+        daten_pd = dl.laden_unterlagen(vzchn, cols_zu_laden)
 
         # print daten_pd['Procurement Name']
-        nl.zeichenen(daten_pd, args.protext[i], 'Procurement Zeichen')
+
+        daten_pd['Procurement String'] = daten_pd[args.protexts[i]].apply(lambda x: ' '.join(x), axis=1)
+        nl.zeichenen(daten_pd, 'Procurement String', 'Procurement Zeichen')
 
     # nl.zeichenen(daten_pd, 'Procurement Name', 'Procurement Zeichen')
-    wortermodell = nl.bauen_wortermodell(daten_pd['Procurement Zeichen'])
+    wortermodell = nl.bauen_wortermodell(daten_pd['Procurement Zeichen'], args.vek_grosse, args.vek_fenster)
 
  
     worter = list(wortermodell.wv.vocab)
     worter.sort()
-    # print(worter)
+    print(worter)
     print('Number of Words:',
           len(worter))
 
     nl.anhangen_wortcodes(daten_pd, wortermodell, 'Procurement Zeichen', 'Procurement Coden')
 
+    print(daten_pd.iloc[234])
+
     print('Number of Procurement lines:',
           len(daten_pd['Procurement Coden']))
+    
     # print(wortermodell['aramark'])
+
+    print("aramark")
+    print(wortermodell.similar_by_vector('aramark', 5))
+    print("wholesale")
+    print(wortermodell.similar_by_vector('wholesale', 5))
+    print("recruitment")
+    print(wortermodell.similar_by_vector('recruitment', 5))
+    print("zurich")
+    print(wortermodell.similar_by_vector('zurich', 5))
 
     print('Done!')
 
